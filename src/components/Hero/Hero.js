864573413d2297d513de428765ffbf15559b2ee6
junaid <js.darkld@gmail.com>
2021-12-12 15:38:03 +0000

    updated CV link

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,7 +15,7 @@ const Hero = (props) => (
         <SectionText>
           I love programming for two basic reasons. One is the feeling when small and chunks of pieces make up to something big and meaningful. This also excites and motivates me to move further and learn more. The other reason is programming can just help you solve your everyday problems. In my case I usually code to automate my recurring tasks. 
         </SectionText>
-        <Button onClick={()=>{window.open('https://drive.google.com/file/d/1EsnmNBk6joLkn7HZr_0qUiNSqL4gUboW/view?usp=sharing')}}>Interested in reading my resume?</Button>
+        <Button onClick={()=>{window.open('https://drive.google.com/file/d/1NmxdSqiRFwG0l-6MLVmVrFEaXF748SO0/view?usp=sharing')}}>Interested in reading my resume?</Button>
       </LeftSection>
     </Section>
   </div>
