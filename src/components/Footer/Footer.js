e483fd741dbba2eba26c61062b52174fb08a2f54
junaid <junaid@cognni.ai>
2025-06-29 23:38:11 +0900

    update number

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,39 +1,48 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyles";
 
 const Footer = () => {
   return (
-  <FooterWrapper>
-    <LinkList>
-      <LinkColumn>
-      <LinkTitle>Call</LinkTitle>
-      <LinkItem href="tel:+92334-0030270">
-      +923340030270
-      </LinkItem>
-      </LinkColumn>
-      <LinkColumn>
-      <LinkTitle>Email</LinkTitle>
-      <LinkItem href="mailto:js.darkld@gmail.com">
-        js.darkld@gmail.com
-      </LinkItem>
-      </LinkColumn>
-    </LinkList>
-    <SocialIconsContainer>
-      <CompanyContainer>
-        <Slogan>
-        A life that lives without doing anything is the same as a slow death.
-        </Slogan>
-      </CompanyContainer>
-      <SocialContainer>
-      <SocialIcons href="https://www.linkedin.com/in/junaid-soomro-063a6a179/">
-          <AiFillLinkedin size="3rem"/>
-        </SocialIcons>
-      </SocialContainer>
-    </SocialIconsContainer>
-  </FooterWrapper>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:+92334-0030270">+818078411087</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:js.darkld@gmail.com">
+            js.darkld@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>
+            A life that lives without doing anything is the same as a slow
+            death.
+          </Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://www.linkedin.com/in/junaid-soomro-063a6a179/">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
